ed8923839270e05b42cda6b626e288e6c7c42462
Evan Flynn <evanflynn.msu@gmail.com>
2020-09-03 12:26:15 -0700

    undo verison bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ package_name = 'grbl_ros'
 
 setuptools.setup(
     name=package_name,
-    version='0.1.1',
+    version='0.1.0',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
