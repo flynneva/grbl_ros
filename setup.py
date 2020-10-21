d6c7b7630634b1620eaa50c55bb8dbe14481673d
Evan Flynn <evanflynn.msu@gmail.com>
2020-10-21 15:28:29 -0700

    bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ package_name = 'grbl_ros'
 
 setuptools.setup(
     name=package_name,
-    version='0.0.12',
+    version='0.0.13',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
