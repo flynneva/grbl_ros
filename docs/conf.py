5e9c20c82cb2ce1fce64403cb5128c35b8a35326
Evan Flynn <evanflynn.msu@gmail.com>
2020-10-21 21:20:20 -0700

    docs work, still need clean up

--- docs/conf.py
+++ docs/conf.py
@@ -38,6 +38,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../'))
 
+autodoc_mock_imports = ['grbl_msgs']
 
 # -- Project information -----------------------------------------------------
 
