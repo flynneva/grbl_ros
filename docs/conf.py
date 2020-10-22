4bf74e65d95bc06b604cc8da75515adae371d030
Evan Flynn <evanflynn.msu@gmail.com>
2020-10-22 07:53:51 -0700

    bumped docs version

--- docs/conf.py
+++ docs/conf.py
@@ -48,7 +48,7 @@ author = 'Evan Flynn'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.0.14'
+release = '0.0.15'
 
 
 # -- General configuration ---------------------------------------------------
