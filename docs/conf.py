cbf0fe57135b2bf6656e5b0816d8a9567415a3c1
Evan Flynn <evanflynn.msu@gmail.com>
2020-10-21 21:26:58 -0700

    added geometry msgs to autodoc

--- docs/conf.py
+++ docs/conf.py
@@ -38,7 +38,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../'))
 
-autodoc_mock_imports = ['grbl_msgs']
+autodoc_mock_imports = ['grbl_msgs', 'geometry_msgs']
 
 # -- Project information -----------------------------------------------------
 
